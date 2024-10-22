0c1863d5b247b9a222ea23f62601eb6378a1db87
LIJHANYI <ovoeasonovo@gmail.com>
2024-10-22 11:37:30 +0800

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,39 @@
+const chartData = {
+    labels: ["碩博士", "高等教育", "高中職", "國中與以下"],
+    data: [6.9, 44, 29.9, 25.5],
+  };
+  
+  const myChart = document.querySelector(".my-chart");
+  const ul = document.querySelector(".programming-stats .details ul");
+  
+  new Chart(myChart, {
+    type: "doughnut",
+    data: {
+      labels: chartData.labels,
+      datasets: [
+        {
+          label: " 佔比\%數",
+          data: chartData.data,
+        },
+      ],
+    },
+    options: {
+      borderWidth: 10,
+      borderRadius: 2,
+      hoverBorderWidth: 0,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+  
+  const populateUl = () => {
+    chartData.labels.forEach((l, i) => {
+      let li = document.createElement("li");
+      li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+      ul.appendChild(li);
+    });
+  };
+  populateUl();
